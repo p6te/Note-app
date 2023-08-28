25e1b8f1ef7d97aa189257473d10b85147b98b17
Piotr Matlak <piotr.matlak.contact@gmail.com>
2023-08-28 19:29:01 +0200

    finish setup of proejct

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <>
-      <p>Hi</p>
-    </>
+    <Routes>
+      <Route path="/" element={<h1>Hi</h1>} />
+    </Routes>
   );
 }
 
